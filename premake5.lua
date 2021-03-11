ee790f786fd62721349522e89151cf6497ebc9d1
Andrea Bucaletti <erbuka@hotmail.com>
2021-03-11 14:31:49 -0500

    --

--- premake5.lua
+++ premake5.lua
@@ -45,37 +45,41 @@ project "GLFW"
     }
     files { 
         "vendor/glfw/src/context.c",
-        "vendor/glfw/src/egl_context.c",
         "vendor/glfw/src/init.c",
         "vendor/glfw/src/input.c",
         "vendor/glfw/src/monitor.c",
-        "vendor/glfw/src/osmesa_context.c",
-        "vendor/glfw/src/vulkan.c",
-        "vendor/glfw/src/wgl_context.c",
         "vendor/glfw/src/window.c",
         "vendor/glfw/src/xkb_unicode.c",
+        "vendor/glfw/src/vulkan.c",
     }
 
     filter "system:windows" 
         defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
         files {
-            "vendor/glfw/src/win32_init.c",
-            "vendor/glfw/src/win32_joystick.c",
-            "vendor/glfw/src/win32_monitor.c",
-            "vendor/glfw/src/win32_thread.c",
-            "vendor/glfw/src/win32_time.c",
-            "vendor/glfw/src/win32_window.c",
+			"vendor/glfw/src/win32_init.c",
+			"vendor/glfw/src/win32_joystick.c",
+			"vendor/glfw/src/win32_monitor.c",
+			"vendor/glfw/src/win32_time.c",
+			"vendor/glfw/src/win32_thread.c",
+			"vendor/glfw/src/win32_window.c",
+			"vendor/glfw/src/wgl_context.c",
+			"vendor/glfw/src/egl_context.c",
+			"vendor/glfw/src/osmesa_context.c"
         }
 
     filter "system:linux"
         defines { "_GLFW_X11 " }
         files {
-            "vendor/glfw/src/x11_init.c",
-            "vendor/glfw/src/x11_monitor.c",
-            "vendor/glfw/src/x11_window.c",  
-            "vendor/glfw/src/posix_thread.c",
-            "vendor/glfw/src/posix_time.c",
-            "vendor/glfw/src/linux_joystick.c",
+			"vendor/glfw/src/x11_init.c",
+			"vendor/glfw/src/x11_monitor.c",
+			"vendor/glfw/src/x11_window.c",
+			"vendor/glfw/src/xkb_unicode.c",
+			"vendor/glfw/src/posix_time.c",
+			"vendor/glfw/src/posix_thread.c",
+			"vendor/glfw/src/glx_context.c",
+			"vendor/glfw/src/egl_context.c",
+			"vendor/glfw/src/osmesa_context.c",
+			"vendor/glfw/src/linux_joystick.c"
         }
     
 project "ImGui"
